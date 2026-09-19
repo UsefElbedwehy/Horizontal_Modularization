acf1466a4fbc9e552ff17ba65da91787d87f46ba
Usef Elbedwehy <elbedwehyy@gmail.com>
2026-09-19 17:59:01 +0300

    add the project

---
+++ HorizontalModularization/HorizontalModularization/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HorizontalModularization
+//
+//  Created by Usef Elbedwehy on 18/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HorizontalModularization/HorizontalModularization/HorizontalModularizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalModularizationApp.swift
+//  HorizontalModularization
+//
+//  Created by Usef Elbedwehy on 18/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalModularizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
