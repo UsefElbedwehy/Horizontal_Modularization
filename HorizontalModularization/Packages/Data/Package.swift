9d20467adde14135b869c7869bce6109237adaba
Usef Elbedwehy <elbedwehyy@gmail.com>
2026-09-19 20:04:35 +0300

    add domain and infrastructure to the data dependencies

--- HorizontalModularization/Packages/Data/Package.swift
+++ HorizontalModularization/Packages/Data/Package.swift
@@ -12,11 +12,13 @@ let package = Package(
             targets: ["Data"]
         ),
     ],
+    dependencies: [.package(path: "../Domain"), .package(path: "../Infrastructure")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Data"
+            name: "Data",
+            dependencies: ["Domain", "Infrastructure"]
         ),
         .testTarget(
             name: "DataTests",
