f3c034e7636bf2ed20bc0f2810689a183deafb5b
Usef Elbedwehy <elbedwehyy@gmail.com>
2026-09-19 20:01:54 +0300

    add domain in the presentation dependencies

--- HorizontalModularization/Packages/Presentation/Package.swift
+++ HorizontalModularization/Packages/Presentation/Package.swift
@@ -12,11 +12,13 @@ let package = Package(
             targets: ["Presentation"]
         ),
     ],
+    dependencies: [.package(path: "../Domain")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Presentation"
+            name: "Presentation",
+            dependencies: ["Domain"]
         ),
         .testTarget(
             name: "PresentationTests",
