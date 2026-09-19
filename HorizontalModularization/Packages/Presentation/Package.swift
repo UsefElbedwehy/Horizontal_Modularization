a6b95f57900ed3d242d6a35b8ba3af806c2c7c93
Usef Elbedwehy <elbedwehyy@gmail.com>
2026-09-19 20:15:51 +0300

    add designSystem as dependency in presentation package

--- HorizontalModularization/Packages/Presentation/Package.swift
+++ HorizontalModularization/Packages/Presentation/Package.swift
@@ -12,13 +12,13 @@ let package = Package(
             targets: ["Presentation"]
         ),
     ],
-    dependencies: [.package(path: "../Domain")],
+    dependencies: [.package(path: "../Domain"), .package(path: "../DesignSystem")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Presentation",
-            dependencies: ["Domain"]
+            dependencies: ["Domain", "DesignSystem"]
         ),
         .testTarget(
             name: "PresentationTests",
